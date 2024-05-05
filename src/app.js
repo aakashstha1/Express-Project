e381f95f131615eeb217f925fd72143361b92f2b
aakashstha1 <aakash.cerestha90@gmail.com>
2024-05-05 15:48:49 +0545

    fifth commit

--- src/app.js
+++ src/app.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const port = 8000;
+const hbs = require("hbs");
+const port = process.env.PORT || 8000;
 
 const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", templatePath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(staticPath));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Home page");
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.send("Welcome to About page");
+  res.render("about");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Welcome to Weather page");
+  res.render("weather");
 });
 
 app.get("*", (req, res) => {
-  res.send("404 error Page not Found");
+  res.render("404error", {
+    errMsg: "Opps! Page Not Found",
+  });
 });
 
 app.listen(port, () => {
